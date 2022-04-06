aeb2ebc769badc64cc4dd44339b6f6df2536c03a
kutsu-4ta <yamashita.bus@gmial.com>
2022-04-06 20:48:10 +0900

    画面遷移でマップ表示

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,86 @@ import 'google_maps.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Maps',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: GoogleMaps(),
+      home: Contents(),
+      // home: FlutterLogin(
+      //   onLogin: (LoginData) {},
+      //   onRecoverPassword: (String) {},
+      // ),
     );
   }
 }
+
+class Contents extends StatelessWidget {
+  final items = List<String>.generate(20, (index) => "$index");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("ホーム画面")),
+      body: Container(
+        width: double.infinity,
+
+        // HACK:リストビュー
+        child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text('${items[index]}'),
+            // subtitle: Image.asset('images/hashibirokou.jpg'),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => GoogleMaps()));
+            },
+          ),
+        ),
+
+        // HACK:グリッドビュー
+        // child:GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        // crossAxisCount: 2,
+        // Generate 100 widgets that display their index in the List.
+        //   children: List.generate(100, (index) {
+        //     return Column(
+        //       children: <Widget>[
+        //         Expanded(
+        //           child: Image.asset('images/hashibirokou.jpg'),
+        //         ),
+        //         Text('Item $index',
+        //             style: Theme.of(context).textTheme.headline5),
+        //       ],
+        //     );
+        //   }),
+        // ),
+
+        // HACK: ライズドボタン
+        // child: RaisedButton(
+        //   child: Text("編集画面"),
+        //   onPressed: () {
+        //     Navigator.push(context, MaterialPageRoute(builder: (context) => EditPage()));
+        //   },
+        // ),
+      ),
+    );
+  }
+}
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Maps',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: GoogleMaps(),
+//     );
+//   }
+// }
