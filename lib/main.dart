64537111f0b2d3cbb36c2eca45bc3d382fb5af55
kutsu-4ta <yamashita.bus@gmial.com>
2022-04-08 00:09:28 +0900

    現在地取れた、デバッグもそのままプッシュしてる、はるほさんありがとう。

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'google_maps.dart';
+// import 'google_maps.dart';
 import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 void main() => runApp(MyApp());
 
@@ -57,36 +58,19 @@ class _HomeState extends State<Home> {
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.navigation),
-            title: Text('Go'),
+            label: 'Go',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.two_wheeler),
-            title: Text('Machine'),
+            label: 'Machine',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.share),
-            title: Text('Share'),
+            label: 'Share',
           ),
         ],
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-      ),
-    );
-  }
-}
-
-class CustomPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20.0))),
-          child: Center(),
-        ),
       ),
     );
   }
@@ -100,6 +84,42 @@ class GoogleMaps extends StatefulWidget {
 class _GoogleMapsState extends State<GoogleMaps> {
   // 非同期処理のときはCompleterを渡す
   Completer<GoogleMapController> _controller = Completer();
+  Location _locationService = Location();
+
+  // 現在位置
+  LocationData? _yourLocation;
+
+  // 現在位置の監視状況
+  StreamSubscription? _locationChangedListen;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getLocation().then((value) {
+      print('aaa');
+      print(value);
+      setState(() {
+        _yourLocation = value;
+      });
+    });
+
+    _locationChangedListen =
+        _locationService.onLocationChanged.listen((LocationData result) async {
+      print(result);
+      setState(() {
+        _yourLocation = result;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    // 監視を終了
+    _locationChangedListen?.cancel();
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
@@ -107,16 +127,48 @@ class _GoogleMapsState extends State<GoogleMaps> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: LatLng(34.643208, 134.997586),
-            zoom: 17.0,
+    if (_yourLocation == null) {
+      // 現在位置が取れるまではローディング中
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      // Google Map ウィジェットを返す
+      return Scaffold(
+        body: Container(
+          child: GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: LatLng(34.643208, 134.997586),
+              zoom: 17.0,
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }
+  }
+
+  Future<LocationData?> _getLocation() async {
+    print('getlocation');
+    // 位置情報サービスが有効かどうかのチェック
+    var serviceEnabled = await _locationService.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await _locationService.requestService();
+      if (!serviceEnabled) {
+        print('service is not enabled');
+        return null;
+      }
+    }
+
+    // 位置情報取得権限チェック
+    var permissionGranted = await _locationService.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await _locationService.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        print('permission is denied');
+        return null;
+      }
+    }
+    return await _locationService.getLocation();
   }
 }
