0c6bafb14c8259d67b89d37d1c2424880ee179be
kutsu-4ta <yamashita.bus@gmial.com>
2022-05-28 02:20:48 +0900

    Machin　ステータス背景

--- lib/machine.dart
+++ lib/machine.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
 
-class Machine extends StatelessWidget {
+class Machine extends StatefulWidget {
+  @override
+  _MachineState createState() => _MachineState();
+}
+
+class _MachineState extends State<Machine> {
   String machineName = 'スーパーカブ';
 
   @override
   Widget build(BuildContext context) {
-    return Text(this.machineName);
+    return LayoutBuilder(builder: (context, contraint) {
+      final height = contraint.maxHeight;
+      final width = contraint.maxWidth;
+
+      //Stackの追加
+      return Stack(children: [
+        // 背景
+        Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.fitHeight,
+                    image: AssetImage('Images/Machine/super_cub.png')))),
+        Container(
+            decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.0, 0.15, 0.4],
+            colors: [
+              Colors.black45,
+              Colors.black54,
+              Colors.black87,
+            ],
+          ),
+        )),
+        // ステータス
+        Container(
+            margin: const EdgeInsets.only(
+              top: 50,
+              left: 200,
+            ),
+            child: Column(children: const [
+              Text('スーパーカブ'),
+              Text('10000km'),
+            ])),
+      ]);
+    });
   }
 }
